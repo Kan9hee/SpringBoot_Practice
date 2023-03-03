1cb57a2edef229a630f6a9663b375c44d241c9d5
Kanghee Han <hcsy1219@gmail.com>
2023-03-03 10:56:13 +0900

    Internationalization

--- src/test/java/practice/validation/message/MessageSourceTest.java
+++ src/test/java/practice/validation/message/MessageSourceTest.java
@@ -7,6 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.MessageSource;
 import org.springframework.context.NoSuchMessageException;
 
+import java.util.Locale;
+
 import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
@@ -36,5 +38,16 @@ public class MessageSourceTest {
     void argumentMessage(){
         String message = messageSource.getMessage("hello.name",new Object[]{"Spring"},null);
         assertThat(message).isEqualTo("안녕 Spring");
+    }
+
+    @Test
+    void defaultLang(){
+        assertThat(messageSource.getMessage("hello",null,null)).isEqualTo("안녕");
+        assertThat(messageSource.getMessage("hello",null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang(){
+        assertThat(messageSource.getMessage("hello",null,Locale.ENGLISH)).isEqualTo("hello");
     }
 }
