e9317cb0d06d272abd4279b973171db4b5571633
Kanghee Han <hcsy1219@gmail.com>
2023-03-15 23:39:25 +0900

    Show error page

--- src/main/java/practice/validation/web/WebServerCustomizer.java
+++ src/main/java/practice/validation/web/WebServerCustomizer.java
@@ -6,15 +6,15 @@ import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class WebServerCustomizer implements WebServerFactoryCustomizer<ConfigurableWebServerFactory> {
     @Override
     public void customize(ConfigurableWebServerFactory factory){
 
-        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND,"/error-page/404");
-        ErrorPage errorPage500 = new ErrorPage(HttpStatus.NOT_FOUND,"/error-page/500");
+        ErrorPage errorPage404 = new ErrorPage(HttpStatus.NOT_FOUND,"/error-404");
+        ErrorPage errorPage500 = new ErrorPage(HttpStatus.NOT_FOUND,"/error-500");
 
-        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class,"/error-page/500");
+        ErrorPage errorPageEx = new ErrorPage(RuntimeException.class,"/error-500");
         // RuntimeException의 자식 예외도 함께 처리한다.
 
         factory.addErrorPages(errorPage404,errorPage500,errorPageEx);
